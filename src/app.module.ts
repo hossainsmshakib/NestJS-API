767b250f11ce8cbdaa711cc1c36f050679730b1f
sm-shakib <smsakib2403@gmail.com>
2023-10-25 13:27:08 +0600

    created user abs bookmark module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UerModule } from './uer/uer.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UerModule, BookmarkModule],
 })
 export class AppModule {}
